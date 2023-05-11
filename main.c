ed4bbd48e219b646a1ee500a8929417cccd4a603
dominic-source <chinonsodomnic@gmail.com>
2023-05-11 12:38:04 -0700

    add boiler plate code v1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - start a simple shell
+ * @ac: count of argument
+ * @av: arguments
+ * Return: -1 or 0
+ */
+int main (int ac, char *av[])
+{
+
+	size_t len = 0;
+	ssize_t line;
+
+	getline(lptr, len, stdin);
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#endif
